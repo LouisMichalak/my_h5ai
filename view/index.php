f45dc8bc6012ac9ea7bd32329bee660c879ef988
Louis Michalak <louis.michalak@epitech.eu>
2019-08-19 12:17:30 +0200

    tests htaccess

---
+++ view/index.php
@@ -0,0 +1,16 @@
+<?php
+include '../php/iteratorPHP/myScandir.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <header>
+    </header>
+    <main>
+        bjr
+    </main>
+</body>
+</html>
